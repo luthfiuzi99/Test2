5366ddbb5df8f10f367afbecc3237ab5b197a808
luthfiuzi <luthfi16stmp@gmail.com>
2024-08-18 23:10:06 +0700

    Add LSP

--- test_lsp.sql
+++ test_lsp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2024 at 10:20 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.28
+-- Generation Time: Aug 18, 2024 at 06:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,13 +29,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tb_mahasiswa` (
   `id` int(3) NOT NULL,
-  `nik` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nama` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tempat_lahir` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `tempat_lahir` varchar(32) NOT NULL,
   `tanggal_lahir` date NOT NULL,
-  `jenis_kelamin` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `agama` enum('islam','kristen','hindu','budha') COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `alamat` text NOT NULL,
+  `agama` enum('islam','kristen','hindu','budha') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -43,10 +43,7 @@ CREATE TABLE `tb_mahasiswa` (
 --
 
 INSERT INTO `tb_mahasiswa` (`id`, `nik`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `agama`) VALUES
-(1, '3291732812738', 'Egi Supriadi', 'Bandung', '2000-07-01', 'laki-laki', 'Bandung jamaras barat', 'islam'),
-(3, '10293891', 'sakjdhajksd', 'bandung', '2024-06-05', 'laki-laki', 'alkjsdksajd', 'islam'),
-(4, '12919873819', 'Egi test 1', 'bandung', '2024-06-11', 'Laki-laki', 'Jamaras Barat bandung', 'islam'),
-(5, '321094819238', 'Test 2', 'Jakarta', '2015-02-02', 'Perempuan', 'Jakarta', 'hindu');
+(6, '213123', 'Luthfi Fauzi', 'Bandung', '2024-08-17', 'Laki-laki', 'Bandung', 'islam');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +63,7 @@ ALTER TABLE `tb_mahasiswa`
 -- AUTO_INCREMENT for table `tb_mahasiswa`
 --
 ALTER TABLE `tb_mahasiswa`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
